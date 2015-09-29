07ee5a120546d7f2345a9c758f9c7d703ddd1d64
vs <hire.vladimir@gmail.com>
2015-09-29 00:47:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+[![PayPal donate button](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=E36NM78QVXA46 "Donate once-off to this project using Paypal")
+
+#Welcome
+This supporting add-on (SA) for Splunk enables lookup of *MAC* address field to IEEE registered vendor information; additional capabilities such as normalization of *MAC* address are also provided per Splunk Common Information model http://docs.splunk.com/Documentation/CIM/latest/User/NetworkTraffic. App comes with sample dashboards to showcase the dataset, however, it can be safely hidden without impacting functionality; details on hiding an app are described at http://docs.splunk.com/Documentation/Splunk/latest/AdvancedDev/ShareYourWork#Set_visibility
+
+Mapping information is obtained from IEEE, found at http://standards.ieee.org/regauth/oui/oui.txt. Additional detail can be found at http://standards.ieee.org/faqs/regauth.html
+
+#Install
+App installation is simple, and only needs to be present on the search head. Documentation around app installation can be found at http://docs.splunk.com/Documentation/AddOns/released/Overview/Singleserverinstall
+
+#Getting Started
+Lookup *mac_vendor_lookup* takes *mac* address an input argument; it performs a case insensitive "starts with" match on the *mac* field to determine vendor information. *mac* field is expected to be normalized per http://docs.splunk.com/Documentation/CIM/latest/User/NetworkTraffic, to help with this effort macro *normalize_mac_address* is provided.
+
+```
+The TCP/IP layer 2 Media Access Control (MAC) address of a packet's source/destination, such as 06:10:9f:eb:8f:14. Note: Always force lower case on this field. Note: Always use colons instead of dashes, spaces, or no separator.
+```
+
+*Note:* Lookup data is static, as in, it is refreshed every app release. It's possible to setup more frequent data refresh, by running the following:
+
+`splunk cmd python SA-mac_vendor/bin/ieee_oui_parser.py > SA-mac_vendor/lookups/mac_vendor_lookup.csv`
+
+## Screenshot
+![mac address to vendor lookup for Splunk ](https://raw.githubusercontent.com/hire-vladimir/SA-mac_vendor/master/static/screenshot.png)
+
+##System requirements
+The app was tested on Splunk 6.2+ on CentOS Linux 7.1.
+
+##Syntax
+`... | `normalize_mac_address(mac)` | lookup mac_vendor_lookup mac OUTPUT mac_vendor, mac_vendor_address, mac_vendor_address2, mac_vendor_country | ...`
+
+##Example
+`| localop | stats count | fields - count | eval src_mac="cc-20-e8-01-ab-3f" | `normalize_mac_address(src_mac)` | lookup mac_vendor_lookup mac AS src_mac OUTPUT mac_vendor, mac_vendor_address, mac_vendor_address2, mac_vendor_country`
+
+#Legal
+* *Splunk* is a registered trademark of Splunk, Inc.
---
+++ bin/ieee_oui_parser.py
@@ -0,0 +1,88 @@
+# /usr/bin/env python
+welcomeText = '''#
+# hire.vladimir@gmail.com
+#
+# takes in http://standards.ieee.org/regauth/oui/oui.txt dataset, makes it pretty csv
+#
+# rev. history
+# 9/28/15 1.0 initial write
+#
+'''
+import time, re
+import logging, logging.handlers
+from urllib2 import urlopen, Request, HTTPError
+import sys
+
+#######################################
+# SCRIPT CONFIG
+#######################################
+# set log level valid options are: (NOTSET will disable logging)
+# CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+LOG_LEVEL = logging.INFO
+OUI_DATASET_URL = "http://standards.ieee.org/regauth/oui/oui.txt"
+
+
+def setup_logging():  # setup logging
+    log_format = "%(asctime)s %(levelname)-s\t%(module)s[%(process)d]:%(lineno)d - %(message)s"
+    logger = logging.getLogger('v')
+    logger.setLevel(LOG_LEVEL)
+
+    # ..and (optionally) output to console
+    logH = logging.StreamHandler()
+    logH.setFormatter(logging.Formatter(fmt=log_format))
+    logger.addHandler(logH)
+
+    logger.propagate = False
+    return logger
+
+
+def die(msg):
+    logger.error(msg)
+    exit(msg)
+
+
+def getDataPayload(uri):
+    logger.debug("Request uri=\"%s\"" % uri)
+    payload = ""
+    try:
+        payload = urlopen(Request(uri)).read()
+        logger.debug('Received payload="%s"' % payload)
+    except HTTPError, e:
+        die('HTTP exception was thrown while making request for uri="%s", status_code=%s, e="%s"' % (uri, e.code, e))
+
+    logger.info('function="getDataPayload" action="success" request="%s", bytes_in="%s"' % (uri, len(payload)))
+    return payload
+
+
+if __name__ == '__main__':
+    logger = setup_logging()
+    logger.info('starting..')
+    eStart = time.time()
+    try:
+        data = ""
+        logger.debug("calling args_count=\"%d\" args=\"%s\"" % (len(sys.argv), str(sys.argv)))
+        if len(sys.argv) > 1:
+            filename = sys.argv[1]
+            if filename[0:1] is not "/":
+                filename = "./%s" % filename
+
+            with open(filename, "r") as oui:
+                data = oui.read()
+        else:
+            data = getDataPayload(OUI_DATASET_URL)
+
+        pattern = "(?P<mac>\w{6})\s+\(base\s16\)\s+(?:(?P<mac_vendor>[^\n]+)\n)(?:\s+(?P<mac_vendor_address>[^\n\r]+)\n)?(?:\s+(?P<mac_vendor_address2>[^\n]+)\n)?(?:\s+(?P<mac_vendor_country>\w{2}))?\n"
+        ma = re.findall(pattern, data.replace("\r", ""))
+
+        logger.debug("there are %d matches" % len(ma))
+
+        print('mac,mac_vendor,mac_vendor_address,mac_vendor_address2,mac_vendor_country')
+        for mac, mac_vendor, mac_vendor_address, mac_vendor_address2, mac_vendor_country in ma:
+            # per http://docs.splunk.com/Documentation/CIM/latest/User/NetworkTraffic
+            normalized_mac = "%s:%s:%s" % (mac[0:2], mac[2:4], mac[4:6])
+            print('"%s*","%s","%s","%s","%s"' % (normalized_mac.lower(), mac_vendor, mac_vendor_address, mac_vendor_address2, mac_vendor_country))
+    except Exception, e:
+        logger.error('error while processing events, exception="%s"' % e)
+        # raise Exception(e)
+    finally:
+        logger.info('exiting, execution duration=%s seconds' % (time.time() - eStart))
